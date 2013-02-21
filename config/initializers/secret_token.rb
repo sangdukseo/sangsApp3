59dc4596d6398a16fcba1d0dae728d1f9d8fa2e4
Sang-Duk Seo <sprintforyourlife@gmail.com>
2013-02-20 21:24:13 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SangsApp3::Application.config.secret_token = 'df7bb655879d46bbf79679d8b223e8d0ee9de1c5a52450620ad4cf12b36ed17baa4ffcde70813bda6527ab75b522f676ea23f6483cab9b9b29a6907186a8298a'
