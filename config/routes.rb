c02410696bb745cc8607e1caee45b56f78c5d661
Sang-Duk Seo <sprintforyourlife@gmail.com>
2013-03-06 16:07:13 -0600

    User registration complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SangsApp3::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Sang-Duk Seo"
+    email    "sprintforyourlife@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
